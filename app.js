9fcab1b19d7483ebfa596024d7e9d241d6f23fff
Sharon Varghese Thoppil <117934101+SharonVargheseThoppil@users.noreply.github.com>
2024-12-04 18:00:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.get('/auth/check-session', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
